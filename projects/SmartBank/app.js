e0756c98a77d953b3296704d4edcce2cf6c777a9
Bavanetha M R <bavanethamr@gmail.com>
2026-02-21 19:20:17 +0530

    error rectified

--- projects/SmartBank/app.js
+++ projects/SmartBank/app.js
@@ -21,7 +21,7 @@ if(signupForm){
         };
         localStorage.setItem("user",JSON.stringify(user));
         alert("Account Created!");
-        window.location.href="login.html";
+        window.location.href="index.html";
     });
 }
 
